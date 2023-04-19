82a65b57276d28d7e0e6e0bccca8ca00dc58c441
Kai Takami <kaitakami@protonmail.com>
2023-04-19 22:25:41 +0900

    Create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <section className='bg-pattern min-h-screen flex items-center justify-center'>
+        <h1 className='text-4xl font-bold'>About Nori Catering</h1>
+      </section>
+    </>
+  )
+}
+
+export default About
